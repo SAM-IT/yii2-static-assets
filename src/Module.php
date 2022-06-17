d3b9258017d2ba0c72dc24f9e1aa621d71f19946
Sam Mousa <sam@mousa.nl>
2022-06-17 15:50:55 +0200

    fix: install envsubst from testing repo2

--- src/Module.php
+++ src/Module.php
@@ -221,7 +221,7 @@ NGINX
         ];
 
         $context->run('apk add --update --no-cache ' . \implode(' ', $packages));
-        $context->run('apk add --update --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/');
+        $context->run('apk add --update --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/ envsubst');
         $context->add('/entrypoint.sh', $this->createEntrypoint());
         $context->run('chmod +x /entrypoint.sh');
         $context->add('/nginx.conf.template', $this->createNginxConfig());
